e8a3cb9c6926a1a993c8a9de26f9c7584c2f9146
r4hul <r4hul@ksu.edu>
2026-02-05 12:47:26 -0600

    Added cache, Fixed with Rebase

--- scripts/3dgrut.sh
+++ scripts/3dgrut.sh
@@ -23,8 +23,9 @@ docker compose --env-file .env -f docker/compose/docker-compose.yml run --rm \
   3dgrut_user \
   conda run -n 3dgrut --no-capture-output \
     python train.py --config-name apps/colmap_3dgrt.yaml \
+    initialization=random initialization.num_gaussians=15625 initialization.xyz_max=2.5 initialization.xyz_min=-2.5 \
     path=/input out_dir=/output experiment_name=3dgrt \
     export_ply.enabled=true export_usdz.enabled=true \
     export_ply.path=/output/export_last.ply export_usdz.path=/output/export_last.usdz \
-    checkpoint.iterations=[2000,4000,6000,8000,10000] \
-    n_iterations=10000
+    checkpoint.iterations=[2000,4000,6000,8000,10000,15000,20000] \
+    n_iterations=20000
